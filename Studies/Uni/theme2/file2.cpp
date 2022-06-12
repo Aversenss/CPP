164be0faf401b1fbd2fcc94933c4a9b603faa1c7
Vladimir Kostikov <86430748+Aversenss@users.noreply.github.com>
2022-06-12 19:07:43 +0300

    Create file2.cpp

---
+++ Studies/Uni/theme2/file2.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+
+using namespace std;
+
+class ClassName {
+    private:
+        int x;
+        int y;
+        static int count;
+    public:
+        ClassName(int _x, int _y): x(_x), y(_y) { count++; };   // num of objects
+        ~ClassName(){};
+        int getCount() { return count; };
+};
+
+int ClassName::count = 0;
+
+int main() {
+    ClassName *coord = new ClassName(2,3);
+    ClassName *coord_2 = new ClassName(4,5);
+    ClassName *coord_3 = new ClassName(6,7);
+
+    std::cout << coord->getCount() << std::endl;
+
+    delete coord;
+    delete coord_2;
+    delete coord_3;
+
+    return 0;
+}
